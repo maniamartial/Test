794c05b9d4272bab9e3c6674ee8069ee96e13c35
maniamartial <martialamania19@gmail.com>
2024-10-13 16:29:26 +0300

    feat - foward payment from sales order to sales invoice

--- nl_apex/hooks.py
+++ nl_apex/hooks.py
@@ -18,7 +18,11 @@ doc_events = {
     "Sales Order": {
         "validate": "nl_apex.apex_piping.overrides.sales_order.validate",
     },
+    "Delivery Note":{
+        "on_submit":"nl_apex.apex_piping.overrides.delivery_note.make_sales_invoice"
+    }
 }
+
 # include js, css files in header of desk.html
 # app_include_css = "/assets/nl_apex/css/nl_apex.css"
 # app_include_js = "/assets/nl_apex/js/nl_apex.js"
