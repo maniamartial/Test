8aeae0eb768ce15dc900c0bca98b83c32eed9e06
maniamartial <martialamania19@gmail.com>
2024-11-18 08:44:34 +0300

    fix - removal of notifier when no price list rate is shown

--- nl_apex/public/js/delivery_note.js
+++ nl_apex/public/js/delivery_note.js
@@ -1,6 +1,6 @@
 frappe.ui.form.on('Delivery Note', {
     after_save: function(frm) {
-        if (!frm.doc.custom_delivery_note_no) {  // Replace 'custom_delivery_note_no' with your custom field name
+        if (!frm.doc.custom_delivery_note_no) { 
             frappe.call({
                 method: "nl_apex.apex_piping.overrides.delivery_note.get_series",
                 args: {
--- nl_apex/public/js/opportunity.js
+++ nl_apex/public/js/opportunity.js
@@ -18,14 +18,12 @@ frappe.ui.form.on("Opportunity Item", "item_code", function(frm, cdt, cdn) {
             if (data.message && data.message.price_list_rate) {
                 var price_rate = data.message.price_list_rate;
 
-                // Set the base rate and rate
                 frappe.model.set_value(cdt, cdn, "base_rate", price_rate);
                 frappe.model.set_value(cdt, cdn, "rate", price_rate);
 
-                // Optional: Refresh the table if needed
                 frappe.refresh_field("items");
             } else {
-                frappe.msgprint(__('No price found for the item.'));
+                console.log('No price found for the item.');
             }
         }
     });
