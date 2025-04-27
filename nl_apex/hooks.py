466052e0d83766fe728fa20a69a2e6a992aedf7f
maniamartial <martialmania19@gmail.com>
2025-04-27 15:02:30 +0300

    feat: pricing rule on opportunity

--- nl_apex/hooks.py
+++ nl_apex/hooks.py
@@ -50,7 +50,10 @@ doc_events = {
     },
     "BOM":{
         "before_save":"nl_apex.apex_piping.overrides.bom.set_cost_per_unit",
-    }
+    },
+    "Opportunity":{
+        "validate":"nl_apex.apex_piping.overrides.opportunity.refresh_opportunity_items_on_validate",
+    },
 }
 
 # include js, css files in header of desk.html
